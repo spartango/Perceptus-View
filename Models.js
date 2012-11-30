583e83eb46f7785fe8a9ab1ce29c0302f02b2773
Anand Gupta <spartango@gmail.com>
2012-11-29 17:36:01 -0800

    stub for models using backbone, not sure if this is the best

---
+++ Models.js
@@ -0,0 +1,23 @@
+/**
+ * ROI
+ *
+ * Model for a region of interest (ROI) 
+ * that's rectangular and associated with a certain feature (tag)
+ * of a particular image (imageId)
+ * 
+ * Can be persisted to and loaded from a server
+ */
+ROI = Backbone.Model.extend({
+    defaults: {
+        imageId: '',
+        tag: '', 
+        x: 0,
+        y: 0,
+        width: 1,
+        height: 1
+    }
+});
+
+ROICollection = Backbone.Collection.extend({
+    model: ROI
+});

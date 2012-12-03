85fb0030f3de688b741eadd72bb5f51cf12a6aed
Andrew Liu <abliu06@gmail.com>
2012-12-02 21:21:47 -0500

    Implemented basic image tileviewer (lost box selector stuff though)
    Also added some extra home stuff should we need it

--- viewApp/app/controllers/annotation_controller.rb
+++ viewApp/app/controllers/annotation_controller.rb
@@ -1,3 +1,22 @@
 class AnnotationController < ApplicationController
-    
-end
+  def segment
+    tile = { :tilejson => '1.0.0', :scheme => 'xyz', :tiles => ['https://s3.amazonaws.com/testconvertedimagebucketserve/' + params[:id] + '/0/{x}/{y}'] }
+    @tilejson = tile.to_json
+    render :layout => false
+  end
+
+#  def classify
+#    @annotation = Annotation.new(params[:annotation])
+#
+#    respond_to do |format|
+#      if @annotation.save
+#        format.html { redirect_to @annotation, :notice => 'Annotation was successfully created.' }
+#        format.json { render :json => @annotation, :status => :created, :location => @annotation }
+#      else
+#        format.html { render :action => "new" }
+#        format.json { render :json => @annotation.errors, :status => :unprocessable_entity }
+#      end
+#    end
+#  end
+
+end 

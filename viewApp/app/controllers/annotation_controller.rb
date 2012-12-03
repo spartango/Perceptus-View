f884bcaaf3d7fc87ebb73fc59ec1133973544b59
Andrew Liu <abliu06@gmail.com>
2012-12-02 23:59:55 -0500

    Updated classify method in controller

--- viewApp/app/controllers/annotation_controller.rb
+++ viewApp/app/controllers/annotation_controller.rb
@@ -5,18 +5,10 @@ class AnnotationController < ApplicationController
     render :layout => false
   end
 
-#  def classify
-#    @annotation = Annotation.new(params[:annotation])
-#
-#    respond_to do |format|
-#      if @annotation.save
-#        format.html { redirect_to @annotation, :notice => 'Annotation was successfully created.' }
-#        format.json { render :json => @annotation, :status => :created, :location => @annotation }
-#      else
-#        format.html { render :action => "new" }
-#        format.json { render :json => @annotation.errors, :status => :unprocessable_entity }
-#      end
-#    end
-#  end
+  def classify
+    tile = { :tilejson => '1.0.0', :scheme => 'xyz', :tiles => ['https://s3.amazonaws.com/testconvertedimagebucketserve/' + params[:id] + '/0/{x}/{y}'] }
+    @tilejson = tile.to_json
+    render :layout => false
+  end
 
 end 

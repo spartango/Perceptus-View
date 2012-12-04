8e200ef5b7934b72a3046ea4af55bc286fc7c472
Anand Gupta <spartango@gmail.com>
2012-12-03 20:47:07 -0800

    Url changes

--- viewApp/app/controllers/annotation_controller.rb
+++ viewApp/app/controllers/annotation_controller.rb
@@ -4,20 +4,20 @@ require 'httparty'
 # HTTParty proxy mapper
 class ImageServer
     include HTTParty
-    base_uri 'localhost:8089'
+    base_uri 'meta.percept.us:8089'
     format :json
 end
 
 class AnnotationController < ApplicationController
   def segment
-    tile = { :tilejson => '1.0.0', :scheme => 'xyz', :tiles => ['https://s3.amazonaws.com/testconvertedimagebucketserve/' + params[:id] + '/0/{x}/{y}'] }
+    tile = { :tilejson => '1.0.0', :scheme => 'xyz', :tiles => ['https://s3.amazonaws.com/testconvertedimagebucketserve2/' + params[:id] + '/0/{x}/{y}'] }
     @imageId = params[:id]
     @tilejson = tile.to_json
     render :layout => false
   end
 
   def classify
-    tile = { :tilejson => '1.0.0', :scheme => 'xyz', :tiles => ['https://s3.amazonaws.com/testconvertedimagebucketserve/' + params[:id] + '/0/{x}/{y}'] }
+    tile = { :tilejson => '1.0.0', :scheme => 'xyz', :tiles => ['https://s3.amazonaws.com/testconvertedimagebucketserve2/' + params[:id] + '/0/{x}/{y}'] }
     @imageId = params[:id]
     @tilejson = tile.to_json
     @rois = ImageServer.get('/image/' + params[:id] + '/rois');
